5b4ace911b4e7e4c447717a8e388dc9e63d38f5f
mario <mario>
2014-06-30 00:59:48 +0000

    Added logout facility, JS links to populate form with a few common OpenID providers, and unsets some $_SESSION fields here instead.

--- page_login.php
+++ page_login.php
@@ -1,8 +1,14 @@
 <?php
 /**
- * OpenID login.
+ * api: freshcode
+ * type: intercept
+ * title: OpenID login
+ * description: Login page shows up for authorization-required sections (e.g. /submit)
+ * version: 0.5
  *
- *
+ * Presents a login box, starts the OpenID auth process.
+ * Has some JS default links for a few identity providers.
+ * Also provides a /logout button now.
  *
  */
 
@@ -19,30 +25,58 @@ if ($_POST->has("login_url")) {
 }
 
 
-// else display form
+// else
 include("layout_header.php");
 ?> <section id=main> <?php
-?>
 
-<h3>Login</h3>
 
-<p>Please provide an <a href="http://en.wikipedia.org/wiki/OpenID">OpenID</a> handle.</p>
+// display login form
+if (empty($_SESSION["openid"])) {
 
-<p>
-<form action="" method=POST class="login box">
-  <input type=text id=login_url name=login_url size=50 value="" placeholder="http://name.openid.xy/">
-  <br>
-  <input type=password style=display:none value=dummy>
-  <input type=submit value=Login>
-  <span class="service-logins">
-     <!--Or with <a onclick="$('#login_url').val('https://www.google.com/accounts/o8/id');">Google</a>-->
-  </span> 
-</form>
-</p>
+    print<<<HTML
+    <h3>Login</h3>
 
-<p>There are intentionally no user accounts on freshcode.club,
-but this prerequisite also helps eschew spam submissions.</p>
+    <p>Please provide an <a href="http://en.wikipedia.org/wiki/OpenID">OpenID</a> handle.</p>
 
-<?php
+    <p>
+    <form action="" method=POST class="login box">
+      <input type=text id=login_url name=login_url size=50 value="" placeholder="http://name.openid.xy/">
+      <br>
+      <input type=password style=display:none value=dummy>
+      <input type=submit value=Login>
+      <span class="service-logins">
+         Or use your <a onclick="$('#login_url').val('http://facebook-openid.appspot.com/YourFaceBookLogin').focus().prop({selectionStart:35, selectionEnd:52});">Facebook</a>
+               | <a onclick="$('#login_url').val('http://me.yahoo.com/#yourname').focus().prop({selectionStart:21, selectionEnd:29});">Yahoo</a> <br>
+               | <a onclick="$('#login_url').val('http://launchpad.net/~yourname').focus().prop({selectionStart:22, selectionEnd:30});">Launchpad</a>
+               | <a onclick="$('#login_url').val('https://openid.stackexchange.com/#yourname').focus().prop({selectionStart:34, selectionEnd:42});">StackOverflow</a>
+      </span> 
+    </form>
+    </p>
+
+    <p>There are intentionally no user accounts on freshcode.club,
+    but this prerequisite also helps eschew spam submissions.</p>
+
+HTML;
+}
+
+// drop relevant session data
+elseif ($_REQUEST->id["name"] == "logout") {
+    $_SESSION["openid"] = "";
+    $_SESSION["user"] = "";
+    print "<h3>Signed out</h3>";
+}
+
+// a previous login was already successful
+else {
+
+    print "<h3>Already logged in</h3>";
+    
+    print isset($login_hint)
+        ? "<p>$login_hint</p>"
+        : "<p>You have already associated an OpenID name.
+           <form action='/login/logout' method=POST><button>Logout</button></form></p>";
+    
+}
+
 include("layout_bottom.php");
 ?>
