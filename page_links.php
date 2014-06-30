887f2f442ffa0f2976a4db6f498b9e5b9e6d7f67
mario <mario>
2014-06-30 00:44:43 +0000

    Link list is now a board of buttons.

--- page_links.php
+++ page_links.php
@@ -4,99 +4,125 @@
  * description: Collection/overview of other software tracking / link lists.
  * version: 0.1
  *
+ *
+ * ToDo
+ *  + http://opensourcelist.org/
+ *  + http://www.linuxsoft.cz/en/
+ *  + http://www.osalt.com/
+ *  + http://www.datamation.com/open-source/open-source-software-the-mega-list-1.html
+ *  + http://distrowatch.com/
+ *
+ *
  */
 
 
 include("layout_header.php");
-?> <section id=main> <?php
+?>
+ <section id=main style="height:2000pt">
 
-
-
-print preg_replace("~(?<![<\">])(https?://[^\s,]+)~", "<a href=\"$1\">$1</a>", <<<HTML
-
-   <h2>Other FLOSS/Linux software directories</h2>
+ <h4>Other FLOSS/Linux software directories</h4>
    <p>
-      <a href="http://freecode.com/">Freecode.com (AKA Freshmeat.net)</a> is only
-      an archive henceforth.
-      But various other software repositories still exist.
-   </p>
-   <p>
-   <ul>
+ <?php
 
-       <li> <a href="http://freshcode.club/">freshcode.club</a>,
-            <a href="http://freecode.club/">freecode.club</a> and
-            <a href="http://freshmeat.club/">freshmeat.club</a> are supposed to become
-            FM/FC substitutes with differing views on shared data sets.
+  $links = [
 
-       <li> <a href="http://directory.fsf.org/">http://directory.fsf.org/</a>
-              - Free Software directory
-              
-       <li> http://www.ohloh.net/
-              - Statistically tracks open source project development.
+      ["http://freecode.com/", "freecode.com.jpeg", "Freecode.com",
+       "(AKA Freshmeat) was the original software release tracker, and is still available as archive."
+      ],
+      ["http://freshcode.club/", "freshcode.club.jpeg", "freshcode.club",
+       "and <a href=\"http://freecode.club/\">freecode</a>/<a href=\"http://freshmeat.club/\">freshmeat.club</a>
+        are supposed to become substitutes with differing views on shared data sets."
+      ],
+      ["http://directory.fsf.org/", "directory.fsf.org.jpeg", "Free Software directory",
+       "is the FSFs Wiki to summarize FLOSS packages and projects."
+      ],
+      ["http://sourceforge.net/", "sourceforge.net.jpeg", "Sourceforge.net",
+       "The original open source development plattform
+        is still home to and primary notification hub for many projects."
+      ],
+      ["http://www.ohloh.net/", "ohloh.net.jpeg", "Ohloh.net",
+       "statistically tracks open source project development."
+      ],
+      ["http://github.com/", "github.com.jpeg", "GitHub",
+       "Is a frontend onto a distributed version control system.
+        It's less suited for end users, but still allows searching for software."
+      ],
+      ["http://savannah.nongnu.org/", "savannah.nongnu.org.jpeg", "Savannah",
+       "Provides an alternative Free software development plattform."
+      ],
+      ["https://launchpad.net/", "launchpad.net.jpeg", "Launchpad",
+       "Is the development hub for Ubuntu and also lists a few things that
+        haven't made it into the package managers yet."
+      ],
+      ["http://www.opensourcesoftwaredirectory.com/", "opensourcesoftwaredirectory.com.jpeg", "Open Source Software Directory",
+       "Lists only stable and well-known Linux software, as it's intended for end users."
+      ],
+      ["http://www.linuxalt.com/", "linuxalt.com.jpeg", "Linux Alternatives",
+       "Curates a list of Linux software alternatives for migrating newcomers."
+      ],
+      ["http://www.libe.net/version/index.php", "libe.net.jpeg", "Libe.net",
+       "Is an archive and version tracker for various Linux and open source packages."
+      ],
+      ["http://en.wikipedia.org/wiki/List_of_free_and_open-source_software_packages", "wikipedia.org.jpeg",
+       "Wikipedia: List of free and open source software packages",
+       "summarizes a few common names."
+      ],
+      ["http://osliving.com/", "osliving.com.jpeg", "Open Source Living",
+       "is an odd outlier, as they expect open source projects to pay for listings;
+        thrives on click thru ads, etc."
+      ],
+      ["http://www.hotscripts.com/", "hotscripts.com.jpeg", "hotscripts.com",
+       "is a script directory, mixed open source or non-free and commercial listings,
+        many entries somewhat outdated"
+      ],
+      ["http://www.devscripts.com/", "devscripts.com.jpeg", "devscripts.com",
+       "is a script directory, mixed open source or non-free and commercial listings,
+        many entries somewhat outdated"
+      ],
+      ["http://www.developertutorials.com/scripts/", "developertutorials.com.jpeg", "developer&shy;tutorials.com",
+       "is a script directory, mixed open source or non-free and commercial listings,
+        many entries outdated"
+      ],
+      ["http://www.bigresource.com/scripts/", "bigresource.com.jpeg", "bigresource.com",
+       "is a script directory, mixed open source or non-free and commercial listings,
+        many entries somewhat outdated"
+      ],
+      ["http://www.scripts.com/", "scripts.com.jpeg", "scripts.com",
+       "is a script directory, mixed open source or non-free and commercial listings,
+        many entries somewhat outdated"
+      ],
+ #     ["http://www.fatscripts.com/", "fatscripts.com.jpeg", "fatscripts.com",
+ #      "is a script directory, mixed open source or non-free and commercial listings,
+ #       many entries somewhat outdated"
+ #     ],
+      ["http://www.scripts20.com/", "scripts20.com.jpeg", "scripts20.com",
+       "is a script directory, mixed open source or non-free and commercial listings,
+        many entries somewhat outdated"
+      ],
+      ["http://www.needscripts.com/", "needscripts.com.jpeg", "needscripts.com",
+       "is a script directory, mixed open source or non-free and commercial listings,
+        many entries somewhat outdated"
+      ],
+      ["http://www.advancescripts.com/", "advancescripts.com.jpeg", "advancescripts.com",
+       "is a script directory, mixed open source or non-free and commercial listings,
+        many entries somewhat outdated"
+      ],
+  ];
+  
+  
+  foreach ($links as $row) {
+      print <<<HTML
+      <div class=links-entry>
+         <a href="$row[0]">
+            <img src="/img/links/$row[1]" width=200 height=150 align=bottom border=0>
+            <b>$row[2]</b>
+         </a>
+         $row[3]
+      </div>
+HTML;
+  }
 
-       <li> http://sourceforge.net/ - The original open source development plattform
-            is still home to and primary notification hub for many projects.
 
-       <li> http://github.com/ - Is a frontend onto a distributed version control
-            system. It's less suited for end users, but still allows searching for
-            software.
-
-       <li> http://savannah.nongnu.org/ - Provides an alternative Free software
-            development plattform.
-
-       <li> https://launchpad.net/ - Is the development hub for Ubuntu and also
-            lists a few things that haven't made it into the package managers yet.
-
-       <li> http://www.opensourcesoftwaredirectory.com/
-              - Lists only stable and well-known Linux software, as it's intended
-                for end users.
-              
-       <li> http://www.linuxalt.com/
-              - Curates a list of Linux software alternatives for migrating newcomers.
-
-       <li> <a href="http://www.libe.net/version/index.php">Libe.net</a>
-              - Linux and open source software archive
-
-       <li> And, of course, WP as always provides a few open source listings
-            as well: http://en.wikipedia.org/wiki/List_of_free_and_open-source_software_packages
-        
-   </ul>
-   </p>
-
-
-   <h2>Script directories, mixed open source/commercial listing</h2>
-   <p>
-       The following listing directories contain web software. For the largest
-       part their code is super ancient, and there are lots of non-free listings.
-   </p>
-   <p>
-   <ul>
-       <li> http://www.hotscripts.com/
-       <li> http://www.devscripts.com/
-       <li> http://www.developertutorials.com/scripts/
-       <li> http://www.bigresource.com/scripts/
-       <li> http://www.scripts.com/
-       <li> http://www.fatscripts.com/
-       <li> http://www.scripts20.com/
-       <li> http://www.needscripts.com/
-       <li> http://www.advancescripts.com/
-
-   </ul>
-   </p>
-
-
-   <h2>Paid-for listings</h2>
-   <ul>
-       <li> http://osliving.com/ - Kinda odd, seems to thrive on click thru
-       ads and is primarily about receiving donations for FLOSS listings.
-   <p>
-   </ul>
-   </p>
-
-      <br><br>TODO: Add screenshots.
-
-HTML
-    );
 
 include("layout_bottom.php");
 
