9e05f176e7434a6115e1350c1ca2bb0b0a073df0
mario <mario>
2014-06-30 01:04:36 +0000

    Utilize HTML5 pattern= regex and maxlength= validation, nicer help texts added, select_form_fields now preserves default non-standard values, $licenses had been moved to config.php, and input form data and field formats are now constrained individually.

--- page_submit.php
+++ page_submit.php
@@ -4,7 +4,7 @@
  * type: page
  * title: Submit/edit project or release
  * description: Single-page edit form for projects and their releases
- * version: 0.2
+ * version: 0.5
  * category: form
  * 
  *
@@ -18,63 +18,88 @@
 
 
 // Form field names
-$fields = array(
+$form_fields = array(
     "name", "title", "homepage", "description", "license", "tags", "image",
     "version", "state", "scope", "changes", "download", "urls",
     "autoupdate_module", "autoupdate_url", "autoupdate_regex",
     "submitter", "lock",
 );
-// License abbreviated names (later adding title= attributes to the select form)
-$licenses = "Apache,Artistic,BSDL,BSDL-2,CDDL,MITL,MPL,Python,PHPL,"
-          . "GNU GPL,GNU GPLv3,GNU LPGL,GNU LPGLv3,Affero GPL,Affero GPLv3,"
-          . "AAL,AFL,APL,APSL,BSL,CeCILL,CATOSL,CPAL,CUA-OPL,EUDatagrid,Eclipse,"
-          . "ECL,Eiffel,Entessa,EUPL,Fair,Frameworx,"
-          . "HPND,IBM PL,IPA,ISC,LaTeX,LPL,MirOS,MS-PL,MS-RL,Motoso,Multics,NASA,"
-          . "NTP,Naumen,NGPL,Nokia,NPOSL,OCLC,OpenFont,OGTSL,OSL,Postgres"
-          . "QPL,RPSL,RPL,Ricoh,SimPL,Sleepycat,Sun PL,Watcom,NCSA"
-          . "VSL,W3C,WXwindows,Xnet,ZPL,Zlib,Other";
-      // todo: discuss adding Commercial/Proprietary?
-      // hint: separation usually works better than prohibition (e.g. filtering instead of moderating)
 
 
 // Start page output
 include("layout_header.php");
-?> <section id=main> <?php
+?> 
+<aside id=sidebar>
+    <section>
+        <h5>Submit project<br>and/or release</h5>
+        <p>
+           You can submit <em title="Free, Libre, and Open Source Software">FLOSS</em>
+           or <em title="or Solaris/Darwin/Hurd">BSD/Linux</em> software here.
+           It's not required that you're a developer of said project.
+        </p>
+        <p>
+           You can always edit the common project information together with
+           a current release.  It will show up on the frontpage whenever you
+           update a new version number and a changelog summary.
+        </p>
+    </section>
+</aside>
+<section id=main>
+<?php
 
 
 
-// Project name from request
-$name = $_REQUEST->nocontrol->trim->name->strtolower->length_3to33["name"];
+/**
+ * Get project ID from request
+ * - only lowercase, must be 3 to 33 letters
+ * - will remain empty for invalid values
+ *
+ */
+$name = $_REQUEST->nocontrol->trim->name->strtolower->length…3…33["name"];
 
-// Check for existing project infos from database
-if ($data = db("SELECT * FROM release WHERE name = ?", $name)->fetch()) {
+
+/**
+ * Check for existing project data in DB.
+ *
+ */
+if ($data = db("SELECT * FROM release WHERE name = ? ORDER BY t_changed DESC", $name)->fetch()) {
     $is_new = 0;
 }
+// Else create new empty $data set
 else {
-    // Create new empty $data set
-    $data = array_combine($fields, array_fill(0, count($fields), ""));
-        #$data = $_REQUEST->list->nocontrol->trim[$fields];
+    $data = array_combine($form_fields, array_fill(0, count($form_fields), ""));
     $is_new = 1;
+    // these fields are pre-defined so they appear with the initial form
     $data["name"] = $name;
     $data["submitter"] = $_SESSION["name"];
     $data["t_published"] = time();
 }
 
 
-// Project entry can be locked for editing by specific OpenIDs
-if (LOGIN_REQUIRED and !$is_new and $data["lock"]
-and !in_array($_SESSION["openid"], array_merge(str_getcsv($data["lock"]), $moderator_ids)))
+/**
+ * Project entry can be locked for editing by specific OpenIDs.
+ * - `lock` can be a comma-separated list
+ * - might also contain password_hash() literals for API auth
+ *
+ */
+if (!$is_new and $data["lock"]
+and !in_array($_SESSION["openid"], array_merge(p_csv($data["lock"]), $moderator_ids)))
 {
-    print "<h3>Locked</h3> <p>This entry cannot be edited with your current login. Its original author registered a different OpenID.</p>";
+    print "<h3>Locked</h3> <p>This entry cannot be edited with your current <a href='/login'>login</a>. Its original author registered a different OpenID.</p>";
 }
 
 
-#-- Fetch form input
+/**
+ * Fetch form input on submit.
+ * Check some constraints.
+ * Then insert into database.
+ *
+ */
 elseif ($name and $_REQUEST->has("title", "description")) {
 
 
     // Check field lengths
-    if (!$_REQUEST->multi->min_length_100["title,description,homepage,changes"]) {
+    if (!$_REQUEST->multi->serialize->length…120…120->strlen["title,description,homepage,changes"]) {
         print("<h3>Submission too short</h3> <p>You didn't fill out crucial information. Please note that our user base expects an enticing set of data points to find your project.</p>");
     }
     // Terms and conditions
@@ -83,25 +108,55 @@ elseif ($name and $_REQUEST->has("title", "description")) {
     }
     // Passed
     else {
+        $_REQUEST->nocontrol->trim->always();
 
-        // Merge input
+        /**
+         * Merge input
+         *
+         */
         $data = array_merge(
+             // any previous/extraneous control data is kept
              $data,
-             $_REQUEST->list->nocontrol->trim[$fields],
+             // format constraints on input fields
+             array(
+                 "name"     => $name,
+                 "homepage" => $_REQUEST->ascii->trim->http  ->length…250["homepage"],
+                 "download" => $_REQUEST->ascii->trim->url   ->length…250["download"],
+                 "image"    => $_REQUEST->ascii->trim->http  ->length…250["image"],
+           "autoupdate_url" => $_REQUEST->ascii->trim->http  ->length…250["autoupdate_url"],
+                 "title"    => $_REQUEST->text               ->length…100["title"],
+              "description" => $_REQUEST                    ->length…2000["description"],
+                 "license"  => $_REQUEST->words               ->length…30["license"],
+                 "tags"     => $_REQUEST->words              ->length…150["tags"],
+                 "version"  => $_REQUEST->words               ->length…30["version"],
+                 "state"    => $_REQUEST->words               ->length…30["state"],
+                 "scope"    => $_REQUEST->words               ->length…30["scope"],
+                 "changes"  => $_REQUEST->text              ->length…2000["changes"],
+                "submitter" => $_REQUEST->words               ->length…30["submitter"],
+                 "urls"     => $_REQUEST                    ->length…2000["urls"],
+                 "lock"     => $_REQUEST->raw               ->length…2000["lock"],
+        "autoupdate_module" => $_REQUEST->id                  ->length…30["autoupdate_module"],
+         "autoupdate_regex" => $_REQUEST->raw               ->length…2000["autoupdate_regex"],
+             ),
+             // some automatic system flags
              array(
                  "t_changed" => time(),
                  "flag" => 0,
                  "submitter_openid" => $_SERVER["openid"],
-                 #"deleted" => 0,
+                 "hidden" => intval($_REQUEST->words->stripos…hidden->is_int["scope"]),
              )
         );
-
+        
+        // Increase associated publishing timestamp if hereunto unknown release
+        if (!project_version_exists($name, $data["version"])) {
+            $data["t_published"] = time();
+        }
+        
         // Update project
-        #db()->test = 1;
         if (db("INSERT INTO release (:?) VALUES (::)", $data, $data)) {
 
             print "<h2>Submitted</h2> <p>Your project and release informations has been saved.</p>
-                  <p>See the result in <a href='http://freshcode.org/projects/$name'>http://freshcode.org/projects/$name</a>.</p>";
+                  <p>See the result in <a href='http://freshcode.club/projects/$name'>http://freshcode.club/projects/$name</a>.</p>";
         }
         else { 
             print "Unspecified error.";
@@ -122,41 +177,32 @@ else {
 
     <form action="" method=POST enctype="multipart/form-data" accept-encoding=UTF-8>
         <input type=hidden name=is_new value=$is_new>
-
-        <h2>Submit project and/or release</h2>
-        <p>
-           You can submit <em title="Free, Libre, and Open Source Software">FLOSS</em>
-           or <em title="or Solaris/Darwin/Hurd">BSD/Linux</em> software here.
-           It's not required that you're a developer of said project.
-        </p>
-        <p>
-           You can always edit the common project information together with
-           a current release.  It will show up on the frontpage whenever you
-           update a new version number and a changelog summary.
-        </p>
-
         
         <h3>General Project Info</h3>
         <p>
            <label>
                Project ID
-               <input name=name size=20 placeholder=projectname value="$data[name]">
+               <input name=name size=20 placeholder=projectname value="$data[name]"
+                      maxlength=33 required>
                <small>A short moniker which becomes your http://freshcode.club/projects/<b>name</b>.</small>
            </label>
 
            <label>
                Title
-               <input name=title size=50 placeholder="Awesome Software" value="$data[title]">
+               <input name=title size=50 placeholder="Awesome Software" value="$data[title]"
+                      maxlength=100 required>
            </label>
 
            <label>
                Homepage
-               <input name=homepage size=50 type=url placeholder="http://project.example.org/" value="$data[homepage]">
+               <input name=homepage size=50 type=url placeholder="http://project.example.org/" value="$data[homepage]"
+                      maxlength=250>
            </label>
 
            <label>
                Description
-               <textarea cols=50 rows=8 name=description>$data[description]</textarea>
+               <textarea cols=50 rows=8 name=description
+                         maxlength=1500 required>$data[description]</textarea>
                <small>Please give a concise roundup of what this software does, what specific features
                it provides, the intended target audience, or how it compares to similar apps.</small>
            </label>
@@ -166,22 +212,23 @@ else {
                <select name=license>
                   {$select($licenses, $data["license"])}
                </select>
-               <small>For abbreviated license names check <a href="http://opensource.org/licenses/alphabetical">OSI list</a>.</small>
+               <small>Again note that FLOSS is preferred.</small>
            </label>
 
            <label>
                Tags
-               <input name=tags size=50 placeholder="game, desktop, gtk, python" value="$data[tags]">
-               <small>Categorize your project using free-form tags. This can include usage context,
-               application type, programming languages, related projects, etc. It's limited to five tags
-               however.</small>
+               <input name=tags size=50 placeholder="game, desktop, gtk, python" value="$data[tags]"
+                      maxlength=150 pattern="^\s*(\w+(-\w+)*(\s*,\s*|\s+)?){0,10}\s*$">
+               <small>Categorize your project. Tags can be made up of letters, numbers and dashes. 
+               This can include usage context, application type, programming languages, related projects,
+               etc.</small>
            </label>
 
            <label>
                Image
-               <input name=image size=50 placeholder="http://i.imgur.com/xyzbar.png" value="$data[image]">
-               <small>Provide a preview image of up to 120x90 px. Supply a path to your website or
-               use e.g. <a href="http://imgur.com/">imgur</a> for uploading.</small>
+               <input type=url name=image size=50 placeholder="http://i.imgur.com/xyzbar.png" value="$data[image]" maxlength=250>
+               <small>Provide a preview image of up to 120x90 px; use <a href="http://imgur.com/">imgur</a> for uploading.
+               It will be fetched and displayed later.</small>
            </label>
         </p>
 
@@ -190,7 +237,7 @@ else {
         <p>
            <label>
                Version
-               <input name=version size=20 placeholder=2.0.1 value="$data[version]">
+               <input name=version size=20 placeholder=2.0.1 value="$data[version]" maxlength=32>
                <small>Prefer <a href="http://semver.org/">semantic versioning</a> for releases.</small>
            </label>
 
@@ -199,7 +246,7 @@ else {
                <select name=state>
                    {$select("initial,alpha,beta,development,prerelease,stable,mature,historic", $data["state"])}
                </select>
-               <small>You can indicate the stability or target audience of the current release.</small>
+               <small>Tells about the stability or target audience of the current release.</small>
            </label>
 
            <label>
@@ -213,19 +260,20 @@ else {
 
            <label>
                Changes
-               <textarea cols=50 rows=7 name=changes>$data[changes]</textarea>
+               <textarea cols=50 rows=7 name=changes maxlength=2000>$data[changes]</textarea>
                <small>Summarize the changes in this release. Documentation additions are as
                crucial as new features or fixed issues.</small>
            </label>
 
            <label>
                Download URL
-               <input name=download size=50 type=url placeholder="http://project.example.org/" value="$data[download]">
+               <input name=download size=50 type=url placeholder="http://project.example.org/" value="$data[download]" maxlength=250>
+               <small>In particular for the download link one could utilize the <b>\$version</b> placeholder.</small>
            </label>
 
            <label>
                Other URLs
-               <textarea cols=50 rows=3 name=urls>$data[urls]</textarea>
+               <textarea cols=50 rows=3 name=urls maxlength=2000>$data[urls]</textarea>
                <small>You can add more project URLs using a comma/newline-separated list
                like <tt>src=http://, deb=http://</tt>.
                Common link types include src=, rpm=, deb=, txz=, dvcs=, forum=, changelog=, etc.</small>
@@ -244,24 +292,24 @@ else {
            <label>
                Via
                <select name=autoupdate_module>
-                   {$select("none,github,sourceforge,regex", $data["autoupdate_module"])}
+                   {$select("none,release.json,github,sourceforge,regex", $data["autoupdate_module"])}
                </select>
            </label>
 
            <label>
                Autoupdate URL
-               <input name=autoupdate_url type=url size=50 value="$data[autoupdate_url]" placeholder="https://github.com/user/project/tags.atom">
-               <small>Add your GitHub tags or Sourceforge project feed URL here.
-               <br>For the Regex method this is the primary source to retrieve and read from.</small>
+               <input name=autoupdate_url type=url size=50 value="$data[autoupdate_url]" placeholder="https://github.com/user/repo/tags.atom" maxlength=250>
+               <small>This is the primary source for <b>releases.json</b> and the <b>regex</b> method.
+               GitHub and Sourceforge URLs are autodiscovered if they're e.g. your project homepage.</small>
            </label>
 
            <label>
                Regex
-               <textarea cols=50 rows=3 name=autoupdate_regex placeholder="version = /-(\d+\.\d+\.\d+)\.txz/">$data[autoupdate_regex]</textarea>
+               <textarea cols=50 rows=3 name=autoupdate_regex placeholder="version = /-(\d+\.\d+\.\d+)\.txz/" maxlength=2500>$data[autoupdate_regex]</textarea>
                <small>
                <a href="http://fossil.include-once.org/freshcode/wiki/AutoupdateRegex">Regex automated updates</a>
                expect a list of field=/regex/ names, like version=, changes=, download=, state=.
-               Like-named "Other URLs" are used alternatively as extraction sources.</small>
+               Associatively-named "Other URLs" are also used for extraction.</small>
            </label>
 
         </p>
@@ -272,13 +320,13 @@ else {
 
            <label>
                Submitter
-               <input name=submitter size=50 placeholder="Your name" value="$data[submitter]">
+               <input name=submitter size=50 placeholder="Your name" value="$data[submitter]" maxlength=50>
                <small>Give us your name or nick name here.</small>
            </label>
 
            <label>
                Lock Entry
-               <input name=lock size=50 placeholder="$_SESSION[openid]" value="$data[lock]">
+               <input name=lock size=50 placeholder="$_SESSION[openid]" value="$data[lock]" maxlength=250>
                <small>Normally all projects can be edited by everyone (WikiStyle).
                If you commit to yours, you can however lock this submission against an OpenID
                handle. (Or even provide a comma-separated list here for multiple contributors.)</small>
@@ -286,8 +334,8 @@ else {
         </p>
         <p>
            <b>Terms and Conditions</b>
-           <label class=inline><input type=checkbox name="req[os]" value=1> It's open source / libre / Free software or pertains BSD/Linux.</label>
-           <label class=inline><input type=checkbox name="req[cc]" value=1> Your entry is shareable under the <a href="http://creativecommons.org/licenses/by-sa/4.0/">CC-BY-SA</a> license.</label>
+           <label class=inline><input type=checkbox name="req[os]" value=1 required> It's open source / libre / Free software or pertains BSD/Linux.</label>
+           <label class=inline><input type=checkbox name="req[cc]" value=1 required> Your entry is shareable under the <a href="http://creativecommons.org/licenses/by-sa/4.0/">CC-BY-SA</a> license.</label>
            <label class=inline><input type=checkbox name="req[sp]" value=1> And it's not spam.</label>
         </p>
         <p>
@@ -308,8 +356,10 @@ include("layout_bottom.php");
 
 // Output a list of select <option>s
 function form_select_options($names, $value, $r="") {
-    foreach (str_getcsv($names) as $id) {
-        $r .= "<option" . ($id == $value ? " selected" : "") . " value=\"$id\">$id</option>";
+    $map = is_string($names) ? array_combine($names = str_getcsv($names), $names) : $names;
+    if ($value and !isset($map[$value])) { $map[$value] = $map[$value]; }
+    foreach ($map as $id=>$title) {
+        $r .= "<option" . ($id == $value ? " selected" : "") . " value=\"$id\" title=\"$title\">$id</option>";
     }
     return $r;
 }
