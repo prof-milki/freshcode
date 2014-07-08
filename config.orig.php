052fa9cda8eca340615444dbb7316ef4c9e510b3
mario <mario>
2014-07-08 09:22:31 +0000

    ./lib/ movement

--- config.orig.php
+++ config.orig.php
@@ -3,34 +3,37 @@
  * api: freshcode
  * title: Freshcode.club config
  * description: initialization code
- * version: 0.2
+ * version: 0.4.2
  * plugin-register: include_once("$FN");
  * 
  *
  */
 
+// input filter
 define("INPUT_QUIET", 1);
 #define("INPUT_DIRECT", "trim");
-include_once("input.php");
+include_once("lib/input.php");
 
-include_once("db.php");
-db("connect", "sqlite:freshcode.db");
-db()->in_clause = 0;
-db()->tokens["fields"]
-     = "name,title,homepage,description,license,tags,version,state,scope,changes,"
-     . "download,urls,autoupdate_module,autoupdate_url,autoupdate_regex,"
-     . "t_published,t_changed,flag,deleted,submitter_openid,submitter,lock,hidden,image";
+// database
+include_once("lib/db.php");
+db(new PDO("sqlite:freshcode.db"));
+#db()->tokens["fields"]
+#     = "name,title,homepage,description,license,tags,version,state,scope,changes,"
+#     . "download,urls,autoupdate_module,autoupdate_url,autoupdate_regex,"
+#     . "t_published,t_changed,flag,deleted,submitter_openid,submitter,lock,hidden,image";
 
+// auth+session
 define("LOGIN_REQUIRED", 0);
 define("HTTP_HOST", $_SERVER->id["HTTP_HOST"]);
 include_once("deferred_openid_session.php");
 
+// utility functions
+include_once("aux.php");
+include_once("release.php");
 
 // List of moderator OpenID handles
 $moderator_ids = array(
 );
-
-include_once("layout_aux.php");
 
 
 ?>
