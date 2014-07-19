9108dab609b656c1d59c27a85e78fef91f7e6d55
mario <mario>
2014-07-19 00:17:03 +0000

    Restructured per-project db() query to be only issued once, fallback message for non-existent pages.

--- page_projects.php
+++ page_projects.php
@@ -4,7 +4,7 @@
  * title: Project detail view
  * description: List project entry with all URLs and releases
  * license: AGPL
- * version 0.4
+ * version 0.5
  * 
  * Shows:
  *   → General project description
@@ -18,56 +18,54 @@
 // Current project id
 $name = $_REQUEST->proj_name["name"];
 
-
-// HTML header with injected RSS/Atom links
-$header_add = "<link rel=alternate type=application/rss+xml href=/feed/$name.rss>\n"
-            . "<link rel=alternate type=application/atom+xml href=/feed/$name.atom>\n"
-            . "<link rel=alternate type=json/vnd.freshcode.club href=/feed/$name.json>";
-$title = "$name - freshcode.club";
-include("template/header.php");
-
-
-
-#-- Fetch most current project/release entry
+#-- Fetch project/release entries
 $releases = db("
-    SELECT *
-      FROM release_versions
-     WHERE name = ?
-       AND NOT deleted
-     LIMIT 1
+        SELECT *, MAX(t_changed)
+          FROM release
+         WHERE name = ?
+           AND flag < 5
+           AND NOT deleted
+      GROUP BY version
+      ORDER BY t_published DESC, t_changed DESC
 ", $name);
 
-// Show sidebar + long project description
+
+// Retrieve most current project version
 if ($entry = $releases->fetch()) {
+
+
+    // prepare HTML header with injected RSS/Atom links
+    $header_add = "<link rel=alternate type=application/rss+xml href=/feed/$name.rss>\n"
+                . "<link rel=alternate type=application/atom+xml href=/feed/$name.atom>\n"
+                . "<link rel=alternate type=json/vnd.freshcode.club href=/feed/$name.json>";
+    $title = input::html($entry["title"]) . " - freshcode.club";
+    include("template/header.php");
+
+
+    // Show sidebar + long project description
     prepare_output($entry);
     include("template/projects_sidebar.php");
     include("template/projects_description.php");
+
+
+    #-- Display all other released versions
+    ?> <article class=release-list>  <h4>Recent Releases</h4> <?php
+    do {
+        prepare_output($entry);
+        include("template/projects_release_entry.php");
+    }
+    while ($entry = $releases->fetch());
+    ?> </article> <?php
+
+    // html tail
+    include("template/bottom.php");
 }
 
 
-
-#-- Retrieve all released versions
-$releases = db("
-    SELECT *, MAX(t_changed)
-      FROM release
-     WHERE name = ?
-       AND flag < 5
-       AND NOT deleted
-  GROUP BY version
-  ORDER BY t_published DESC, t_changed DESC
-", $name);
-
-?> <article class=release-list>  <h4>Recent Releases</h4> <?php
-while ($entry = $releases->fetch()) {
-    prepare_output($entry);
-    include("template/projects_release_entry.php");
+// No entry found
+else {
+    exit($error = "Project name doesn't exist." and include("page_error.php"));
 }
-?> </article> <?php
-
-
-// html tail
-include("template/bottom.php");
-
 
 
 ?>
