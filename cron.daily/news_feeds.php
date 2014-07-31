241bf1eb422c0618ff80a432196b0593ed5e4d4b
mario <mario>
2014-07-31 21:56:58 +0000

    added DistroWatch feed and general version number highlighting

--- cron.daily/news_feeds.php
+++ cron.daily/news_feeds.php
@@ -2,7 +2,10 @@
 /**
  * title: Article feeds
  * description: Queries a few online resources for article links
- * version: 0.1
+ * version: 0.4
+ *
+ * Highlights version numbers in news feeds,
+ * and populates templates/feed.*.htm for sidebar display.
  *
  */
 
@@ -17,8 +20,11 @@ $feeds = array(
     "linuxcom" => "http://www.linux.com/news/software?format=feed&type=rss",
     "linuxgames" => "http://www.linuxgames.com/feed",
     "sourceforge" => "http://sourceforge.net/directory/release_feed/",
+    "distrowatch" => "http://distrowatch.com/news/dwd.xml",
 );
-
+$filter = array(
+    "Please 'report' off-topic submissions and spam comments.",
+);
 
 #-- Traverse and collect entries
 foreach ($feeds as $name=>$url) {
@@ -32,27 +38,45 @@ foreach ($feeds as $name=>$url) {
     // append
     $i = 0;
     foreach ($x->channel->item as $item) {
+    
+        # pre-filter
+        list($title, $link) = array( htmlspecialchars($item->title),  htmlspecialchars($item->link) );
+        if (empty($title) or empty($link) or in_array($title, $filter)) {
+            continue;
+        }
 
+        # per feed
         switch ($name) {
-        
+
+            // Extract project base names and version numbers
             case "sourceforge":
                 if (preg_match("~^(http://sourceforge.net/projects/(\w+))/files/.+?(\d+(\.\d)+).+?/download$~", $item->link, $m)) {
                     $html .= "<a href=\"$m[1]\">$m[2] <em>$m[3]</em></a>\n";
-                    if ($i++ >= 20) { break 2; }
+                    $i++;
                 }
-            break;
+                break;
 
+            // Extract project base names and version numbers
+            case "distrowatch":
+                if (preg_match("~^(\d+/\d+)\s(\D+)\s+(.+)$~", $title, $m)) {
+                    $html .= "<a href=\"$link\"><small style=color:grey>$m[1]</small> $m[2] <em>$m[3]</em></a>\n";
+                }
+                break;
+
+            // Titles as is
             default:
             case "reddit":
             case "linuxcom":
             case "linuxgames":
                 if (strlen($item->link) and strlen($item->title)) {
-                    $html .="<a href=\"" . htmlspecialchars($item->link)
-                        . "\">" . htmlspecialchars($item->title) . "</a>"
-                        . "\n";
-                    if ($i++ >= 20) { break 2; }
+                    $title = preg_replace("~(\d+\.[\d-.]+)~", "<em>$0</em>", $title);
+                    $html .="<a href=\"$link\">$title</a>\n";
+                    $i++;
                 }
+                break;
         }
+
+        if ($i >= 20) { break; }
     }
     
     // save
