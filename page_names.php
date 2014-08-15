f224650894847c6bb6cd9869af8c793bb19b5e70
mario <mario>
2014-08-15 12:37:45 +0000

    Split up letter ranges (O-Q and R-T)

--- page_names.php
+++ page_names.php
@@ -3,15 +3,26 @@
  * type: page
  * title: Browse Projects by Name
  * description: Alphabetical project lists
- * version: 0.1
+ * version: 0.2
  *
- * Needs some styling, too early for splitting up letter ranges.
+ * Minimal column styling, just project base names used,
  *
  */
 
 
 include("template/header.php");
-?><section id=main><article class=project-name-columns><?php
+?><section id=main>
+</article>
+<aside class=pagination-links style="margin-bottom: 20pt;">
+  <a href="names/AE">A-E</a>
+  <a href="names/FH">F-H</a>
+  <a href="names/IN">I-N</a>
+  <a href="names/OQ">O-Q</a>
+  <a href="names/RT">R-T</a>
+  <a href="names/UZ">U-Z</a>
+  <a href="names/09">0-9</a>
+</aside>
+<article class=project-name-columns><?php
 
 
 // Letter slicing (AZ or 09)
@@ -31,18 +42,6 @@ foreach ($names as $id) {
     print "<a href=/projects/$id[name]><img src='img/screenshot/$id[name].jpeg' width=100 height=75 align=top> $id[name]</a> <br> ";
 }
 
-
-?>
-</article>
-<aside class=pagination-links>
-  <a href="names/AE">A-E</a>
-  <a href="names/FH">F-H</a>
-  <a href="names/IN">I-N</a>
-  <a href="names/OT">O-T</a>
-  <a href="names/UZ">U-Z</a>
-  <a href="names/09">0-9</a>
-</aside>
-<?php
 
 include("template/bottom.php");
 
