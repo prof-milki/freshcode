0dbe870e44e0fa128dde802ea333caf819d03b72
mario <mario>
2014-08-24 01:17:21 +0000

    Display matching project (per tag or exact search phrase) as full release entry instead of search excerpt.

--- page_search.php
+++ page_search.php
@@ -37,19 +37,21 @@ else {
     $user = $_GET->words["user"] and $user = ["$user%"];
     $license = $_GET->array->words["license"] and $license = array_filter($license);
     $search = $_GET->text["q"] and $search = ["%$search%"];
+    // primary search term (for exact matches)
+    $primary = (count($tags) == 1) ? current($tags) : ($search ? $_GET->text["q"] : NULL);
 
     // Run SQL
 #   db()->test = 1;
     $result = db("
         SELECT release.name AS name, title, SUBSTR(description,1,500) AS description,
                version, image, homepage, download, submitter, submitter_image,
-               release.tags AS tags,
+               release.tags AS tags, scope, changes,
                license, state, t_published, flag, hidden, deleted, MAX(t_changed)
           FROM release
          WHERE NOT deleted AND flag < 5
       GROUP BY release.name
         HAVING 1=1
-               :*  :*  :*  :*  :*
+               :*  :*  :*  :*  :*  :*
       ORDER BY t_published DESC, t_changed DESC
          LIMIT 100 ",
             // expr :* placeholders only interpolate when inner array contains params
@@ -58,7 +60,9 @@ else {
             [" AND license IN (??) ",   $license],
             [" AND name IN (SELECT name FROM tags WHERE tag IN (??)) ", $tags],
             [" AND name IN (SELECT name FROM tags WHERE tag IN (??)
-               GROUP BY name HAVING COUNT(tag) = 1*?) ", $trove]
+               GROUP BY name HAVING COUNT(tag) = 1*?) ", $trove],
+            // project name == tags
+            [" OR name IN (??)", $tags]
     );
 
 
@@ -66,7 +70,15 @@ else {
     // Show sidebar + long project description
     foreach ($result as $entry) {
         prepare_output($entry);
-        include("template/search_entry.php");
+        
+        // show project listing for exact match or search entry
+        if ($entry["name"] == $primary) {
+            include("template/index_project.php");
+            print "<br>";
+        }
+        else {
+            include("template/search_entry.php");
+        }
     }
 
 }
