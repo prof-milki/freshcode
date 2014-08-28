dd97dd05e74a5f2d71febbb667f62efc4e868bc0
mario <mario>
2014-08-28 18:51:27 +0000

    Reordered index sidebar blocks (new fossies.org links)

--- template/index_sidebar.php
+++ template/index_sidebar.php
@@ -20,9 +20,19 @@
         <?php  include("template/feed.linuxcom.htm");  ?>
     </section>
 
+    <section class="article-links untrimmed feed-fossies">
+        <a href="http://fossies.org/"><h5><span class=green>Fossies</span>.org <small class=blue>/misc</small></h5></a>
+        <?php  include("template/feed.fossies.htm");  ?>
+    </section>
+
     <section class="article-links trimmed">
         <h5>reddit<em>/r/linux</em></h5>
         <?php  include("template/feed.reddit.htm");  ?>
+    </section>
+
+    <section class="article-links untrimmed">
+        <h5>DistroWatch</h5>
+        <?php  include("template/feed.distrowatch.htm");  ?>
     </section>
 
     <section class="article-links trimmed">
@@ -33,11 +43,6 @@
     <section class="article-links untrimmed">
         <h5>Sourceforge Files</h5>
         <?php  include("template/feed.sourceforge.htm");  ?>
-    </section>
-
-    <section class="article-links untrimmed">
-        <h5>DistroWatch</h5>
-        <?php  include("template/feed.distrowatch.htm");  ?>
     </section>
 
  </aside>
