447c5a6f38a47d4b52012cf5a4c2b16d58f045f8
mario <mario>
2014-11-25 16:51:41 +0000

    Switch uneeded include() for readfile(), all feed templates are static.

--- template/index_sidebar.php
+++ template/index_sidebar.php
@@ -17,45 +17,45 @@
 
     <section class="article-links untrimmed">
         <h5>Linux.com Software</h5>
-        <?php  include("template/feed.linuxcom.htm");  ?>
+        <?php  readfile("template/feed.linuxcom.htm");  ?>
     </section>
 
     <section class="article-links untrimmed feed-fossies">
         <a href="http://fossies.org/"><h5><span class=green>Fossies</span>.org <small class=blue>/misc</small></h5></a>
-        <?php  include("template/feed.fossies.htm");  ?>
+        <?php  readfile("template/feed.fossies.htm");  ?>
     </section>
 
     <section class="article-links trimmed">
         <h5>reddit<em>/r/linux</em></h5>
-        <?php  include("template/feed.reddit.htm");  ?>
+        <?php  readfile("template/feed.reddit.htm");  ?>
     </section>
 
     <section class="article-links untrimmed">
         <h5>DistroWatch</h5>
-        <?php  include("template/feed.distrowatch.htm");  ?>
+        <?php  readfile("template/feed.distrowatch.htm");  ?>
     </section>
 
     <section class="article-links trimmed">
         <h5>Games <a href="http://www.linuxgames.com/" style=display:inline>LG</a>, <a href="http://www.gamingonlinux.com/" style=display:inline>GoL</a>, <a href="http://freegamer.blogspot.com/" style=display:inline>FG</a></h5>
-        <?php  include("template/feed.gamingonlinux.htm");  ?>
-        <?php  include("template/feed.linuxgames.htm");  ?>
-        <?php  include("template/feed.linuxgamenews.htm");  ?>
-        <?php  include("template/feed.freegamer.htm");  ?>
+        <?php  readfile("template/feed.gamingonlinux.htm");  ?>
+        <?php  readfile("template/feed.linuxgames.htm");  ?>
+        <?php  readfile("template/feed.linuxgamenews.htm");  ?>
+        <?php  readfile("template/feed.freegamer.htm");  ?>
     </section>
 
     <section class="article-links untrimmed">
         <h5>Sourceforge Files</h5>
-        <?php  include("template/feed.sourceforge.htm");  ?>
+        <?php  readfile("template/feed.sourceforge.htm");  ?>
     </section>
 
     <section class="article-links trimmed">
         <h5>beOpen</h5>
-        <?php  include("template/feed.beopen.htm");  ?>
+        <?php  readfile("template/feed.beopen.htm");  ?>
     </section>
 
     <section class="article-links trimmed">
         <a href="/github-releases"><h5>GitHub releases →</h5></a>
-        <?php  include("template/feed.github.htm");  ?>
+        <?php  readfile("template/feed.github.htm");  ?>
     </section>
 
  </aside>
